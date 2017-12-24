0c4b3b5f5e29b26f1c484f97f204d76fda45dee2
voxelru <muro_hasegava@mail.ru>
2017-12-24 14:52:01 +0300

    Stability improvements and commands recognisation

--- app.py
+++ app.py
@@ -8,7 +8,17 @@ bot = telebot.TeleBot(token)
 
 @bot.message_handler(content_types=["text"])
 def repeat_all_messages(message): # Название функции не играет никакой роли, в принципе
-    bot.send_message(message.chat.id, 'You wrote: ' + message.text)
+    if message.text[0] == '/':
+        bot.send_message(message.chat.id, 'Command: ' + message.text)
+    else:
+        bot.send_message(message.chat.id, 'You wrote: ' + message.text)
 
 if __name__ == '__main__':
-     bot.polling(none_stop=True)
+
+    while True:
+        try:
+            bot.polling(none_stop=True)
+        except Exception as e:
+            print(e)
+            continue
+
