28be8321ffd79f05bd744451a515a8e15b9ee57d
voxelru <muro_hasegava@mail.ru>
2017-12-25 23:48:53 +0300

    usability improvement

--- app.py
+++ app.py
@@ -2,13 +2,17 @@
 
 
 # !/usr/bin/python3
+from math import log
 import requests
 import time
 from pymongo import MongoClient
 import sys
 
-#import config
+import pandas as pd
+
+from config import token
 import telebot
+from telebot import types
 import time
 
 
@@ -76,57 +80,85 @@ def get_orderbook(url):
     response.raise_for_status()
     return response.json()
 
-
-token = '478680072:AAHLM8h9Y-pYXkn-THQA-YoVeI7ueo1lhw0'
 bot = telebot.TeleBot(token)
+
+
+# Обработчик команд '/start' и '/help'.
+@bot.message_handler(commands=['start', 'help'])
+def handle_start_help(message):
+    keyboard = types.InlineKeyboardMarkup()
+
+#    url_button = types.InlineKeyboardButton(text="Перейти на Яндекс", url="https://ya.ru")
+#    keyboard.add(url_button)
+#    bot.send_message(message.chat.id, "Привет! Нажми на кнопку и перейди в поисковик.", reply_markup=keyboard)
+
+    markup = types.ReplyKeyboardMarkup()
+    markup.row('/btc', '/zec')
+    markup.row('/xrp', '/eth', '/bch')
+    bot.send_message(message.chat.id, "Choose currency:", reply_markup=markup)
+    pass
+
+
 @bot.message_handler(content_types=["text"])
 def repeat_all_messages(message): # Название функции не играет никакой роли, в принципе
-    if message.text[0] == '/':
-        bot.send_message(message.chat.id, 'Command: ' + message.text)
+
+
+    if message.text[0] == '/' and message.text != '/start':
+
+
+#        bot.send_message(message.chat.id, 'Command: ' + message.text)
 
         api = 'https://api.bitfinex.com/v1'
         symbol = message.text[1:4]
-        limit = 50
-        url = '{0}/book/{1}usd?limit_bids={2}&limit_asks={2}' \
-            .format(api, symbol, limit)
 
-        start = time.time()
-        try:
-            orderbook = get_orderbook(url)
-            orderbook = format_orderbook(orderbook)
+        if symbol in ('btc', 'zec', 'bch', 'xrp', 'eth'):
 
-            orderbook['_id'] = time.time()
-            #      ltc_orderbooks.insert_one(orderbook)
 
-            book = Book()
+            limit = 500
+            url = '{0}/book/{1}usd?limit_bids={2}&limit_asks={2}' \
+                .format(api, symbol, limit)
 
-            mid = book.mid
-            width = book.width
-            power_imbalance_2, power_imbalance_4, power_imbalance_8 = book.power_imbalance_list
-            power_adjusted_price_2, power_adjusted_price_4, power_adjusted_price_8, = book.power_adjusted_prices
-            print(("mid: %s\n"
-                   "width: %s\n"
-                   "power_imbalance_2: %s\n"
-                   "power_imbalance_4: %s\n"
-                   "power_imbalance_8: %s\n"
-                   "power_adjusted_price_2: %s\n"
-                   "power_adjusted_price_4: %s\n"
-                   "power_adjusted_price_8: %s")
-                  % (mid,
-                     width,
-                     power_imbalance_2,
-                     power_imbalance_4,
-                     power_imbalance_8,
-                     power_adjusted_price_2,
-                     power_adjusted_price_4,
-                     power_adjusted_price_8,
-                     ))
+            start = time.time()
+            try:
 
-        except Exception as e:
-            print(e)
+                orderbook = get_orderbook(url)
+                orderbook = format_orderbook(orderbook)
+
+                orderbook['_id'] = time.time()
+                #      ltc_orderbooks.insert_one(orderbook)
+
+                book = Book(orderbook)
+
+                mid = book.mid
+                width = book.width
+                power_imbalance_2, power_imbalance_4, power_imbalance_8 = book.power_imbalance_list
+                power_adjusted_price_2, power_adjusted_price_4, power_adjusted_price_8, = book.power_adjusted_prices
+                bot.send_message(message.chat.id, symbol.upper()+' features:\n' + ("MID: %s\n"
+                       "WID: %.2f\n"
+                       "PI2: %.2f\n"
+                       "PI4: %.2f\n"
+                       "PI8: %.2f\n"
+                       "PA2: %.2f\n"
+                       "PA4: %.2f\n"
+                       "PA8: %.2f")
+                      % (mid,
+                         width,
+                         power_imbalance_2,
+                         power_imbalance_4,
+                         power_imbalance_8,
+                         power_adjusted_price_2,
+                         power_adjusted_price_4,
+                         power_adjusted_price_8,
+                         )+'\n'+time.strftime("%a, %d %b %Y %H:%M:%S", time.gmtime(time.time()+180*60)))
+
+            except Exception as e:
+               print(e)
+
+        else:
+            bot.send_message(message.chat.id, 'The available commands are: \n/btc\n/zec\n/bch\n/xrp\n/eth')
 
     else:
-        bot.send_message(message.chat.id, 'You wrote: ' + message.text)
+        bot.send_message(message.chat.id, 'The available commands are: \n/btc\n/zec\n/bch\n/xrp\n/eth')
 
     print(time.strftime("%a, %d %b %Y %H:%M:%S", time.gmtime(time.time())) + ' Added one: '+message.text)
 
